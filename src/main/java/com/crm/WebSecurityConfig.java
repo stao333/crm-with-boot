9cfdd91705549c1ed914dd00950dc4f222321101
stao333 <stao333@gmail.com>
2017-04-11 11:57:00 +0100

    Disabled csrf to allow ajax to work.

--- src/main/java/com/crm/WebSecurityConfig.java
+++ src/main/java/com/crm/WebSecurityConfig.java
@@ -15,7 +15,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
 	@Override
 	protected void configure( HttpSecurity http ) throws Exception {
 		http.authorizeRequests().antMatchers( "/" ).permitAll().anyRequest().authenticated().and().formLogin().loginPage( "/login" )
-				.permitAll().and().logout().permitAll();
+				.permitAll().and().logout().permitAll().and().csrf().disable();
 	}
 
 	@Autowired
